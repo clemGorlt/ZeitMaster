4342983f48b66f89a9d569df6a29eb4a605480d7
Clement Gorlt <Clement.gorlt@wanadoo.fr>
2014-03-28 15:28:06 +0100

    guess time derivation in the capture via http response

--- ini_test/r.py
+++ ini_test/r.py
@@ -3,11 +3,6 @@ from datetime import datetime, timedelta
 import sys
 import locale
 
-class tuple(object):
-	def __init__(self,numb,deriv):
-		self.number=numb
-		self.derivation=deriv
-
 def strToDate(arg,fmt):
 	locale.getdefaultlocale()
 	try:
@@ -20,31 +15,33 @@ def strToDate(arg,fmt):
 			raise e
 	return arg
 
-def printArray(array):
-	print('Array of derivation: ')
-	for d in array:
-		print(d.number,"|",d.derivation)
-
-def processArray(array):
-	print('processing the array...')
-
+def processList(array,bound=24):
+	print('processing the list...')
+	d=0
 	minDiff=timedelta(days=2)#suppose to be max possible delta
 	maxDiff=timedelta(microseconds=0)
 	avgDiff=timedelta(microseconds=0)
-	for d in array:
-		if d.derivation>maxDiff:
-			maxDiff=d.derivation
-		if d.derivation<minDiff:
-			minDiff=d.derivation
-		avgDiff+=d.derivation
-
-	avgDiff/=len(array)
-	print('finished')
+	bounds=timedelta(hours=bound)
+	skip=0
+	for elt in array:
+		if elt > bounds:
+			skip+=1
+			array.remove(elt)
+			continue
+		if elt >maxDiff:
+			maxDiff=elt 
+		if elt <minDiff:
+			minDiff=elt
+	avgDiff=sum(array,timedelta(0))/len(array)
+	print('... finished: skiped ',skip,'element(s)')
+	print('Min < Average < Max:')
 	print(minDiff,'<',avgDiff,'<',maxDiff)
-
+	avgDiff=avgDiff.seconds%3600 #hours are due to the different Timezone, this is not what we are looking for
+	return avgDiff
 fmt='%b %d, %Y %H:%M:%S.%f'
 fmt_HTTP='%a, %d %b %Y %H:%M:%S %Z'#example: Sun, 16 Sep 2012 12:50:01 GMT
-derivArray=[]
+derivList=[]
+cptList=0
 argParser = argparse.ArgumentParser(description='Zeitmaster: pcap time Analyser')
 argParser.add_argument('-f',action='append', help='Filename')
 args = argParser.parse_args()
@@ -53,37 +50,36 @@ if args.f is not None:
 	i=0
 	debut=0
 	fin=0
+	print("BEGIN\n processing data from pcap")
 	for line in sys.stdin:
-		print("BEGIN\n processing data from pcap")
-		print(line)
+		
+		#print(line)
 		#frame.time/ip.proto/udp.port/tcp.port/http.response->1 true|0 false/http.date/frame.number
 		tab=line.split("\\")
-		#print(tab[1])
 		if i==1:
 			debut=tab[0]
 		#example for the next ntp-search
-		if tab[2]=='68,67\n': # this line is DHCP
-			print("DHCP")
-		
+		#if tab[2]=='68,67\n': # this line is DHCP
+			#print("DHCP")
 		if '80' in tab[3] or '443' in tab[3]: # let's say http can only use tcp
-			print("HTTP")
-			print(repr(tab[4]))
 			if tab[4]=='1':
 				frame=strToDate(tab[0],fmt)
 				http=strToDate(tab[5],fmt_HTTP)
-				d=tuple(tab[6].rstrip('\n'),(frame-http))
-				derivArray.append(d)
-		i=i+1
+				#d=tuple(tab[6].rstrip('\n'),(frame-http))
+				derivList.insert(cptList,(frame-http))
+				cptList+=1
+		i+=1
 		fin=tab[0]
-		print("finished processing data from pcap")
+	print("finished processing data from pcap")
 	print(" debut/fin: ",debut,"/",fin)
 	#print(repr(fin))
 	debut=strToDate(debut,fmt)
 	fin=strToDate(fin,fmt)
 	print("delta:",(fin-debut))
 	
-	processArray(derivArray)
-	printArray(derivArray)	
+	avg=processList(derivList)
+	print("avg derivation:",avg,"sec(s)")
+	#printArray(derivArray)	
 		
 else:
 	print("file not found ")
--- ini_test/t.sh
+++ ini_test/t.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#cd ../sample/
-#ls -1 . | parallel --gnu "cat {1} | tshark -E separator=/ -E header=yes -Tfields -e frame.time -e ip.src -e ip.dst -r {1}" | python ../ini_test/r.py -f {1} >> test.txt
-
 usage(){
 	echo "usage: $0 file"
 }
@@ -11,6 +8,6 @@ if [ $# -ne 1 ];then
 	usage
 	exit 1
 fi
-editcap -t -3600 $1 $1
+#editcap -t -3600 $1 $1
 tshark -E separator=/ -E header=yes -Tfields -e frame.time -e ip.proto -e udp.port -e tcp.port -e http.response -e http.date -e frame.number -r $1 | python ../ini_test/r.py -f $1
 #bla
