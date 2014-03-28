1ff2fc36918ad4bbbc24a3cee18266bb782c632c
Clement Gorlt <Clement.gorlt@wanadoo.fr>
2014-03-28 12:23:30 +0100

    Delete the Time derivation due to timezone

--- ini_test/t.sh
+++ ini_test/t.sh
@@ -11,5 +11,6 @@ if [ $# -ne 1 ];then
 	usage
 	exit 1
 fi
+editcap -t -3600 $1 $1
 tshark -E separator=/ -E header=yes -Tfields -e frame.time -e ip.proto -e udp.port -e tcp.port -e http.response -e http.date -e frame.number -r $1 | python ../ini_test/r.py -f $1
 #bla
